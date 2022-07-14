beec1b50fff2ca18020c5763f55c5d2c79b16cce
Murilo Beltrame <murilobeltrame@gmail.com>
2022-07-14 09:39:43 -0300

    added BP Api

---
+++ src/backend/BusinessPartners.Api/src/main/java/com/murilobeltrame/businesspartnersapi/BusinessPartnersApiApplication.java
@@ -0,0 +1,13 @@
+package com.murilobeltrame.businesspartnersapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessPartnersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessPartnersApiApplication.class, args);
+	}
+
+}
---
+++ src/backend/BusinessPartners.Api/src/test/java/com/murilobeltrame/businesspartnersapi/BusinessPartnersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murilobeltrame.businesspartnersapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessPartnersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
